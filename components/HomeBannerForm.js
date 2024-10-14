2730cbde53c902736156e1726ff279ea99ff89f0
LMLHOMESLLP <157579608+LMLHOMESLLP@users.noreply.github.com>
2024-10-14 17:31:53 +0530

    HomeBannerForm.js

--- components/HomeBannerForm.js
+++ components/HomeBannerForm.js
@@ -57,7 +57,7 @@ export default function HomeBannerForm({ closeForm }) {
                         <input type='text' className={styles.formInput} placeholder='* Name' required value={name} onChange={(e) => setName(e.target.value)} name='Name' />
                         <input type='text' className={styles.formInput} placeholder='* Email' required value={email} onChange={(e) => setEmail(e.target.value)} name='Email' />
                         <input type='text' className={styles.formInput + ' pb-5'} placeholder='* Message' required value={message} onChange={(e) => setMessage(e.target.value)} name='Message'/>
-
+<p style={{fontSize: '11px',  width: '250px', marginTop: '10px'}}><img src='https://www.lmlhomes.in/assets/images/tick-mark.jpg' height={12} /> I authorise LML Homes LLP & its representatives to contact me with updates and notifications via Email / SMS /WhatsApp / Call. This will override DND/NDNC. I accept the <a href='https://www.lmlhomes.in/privacy-policy' target='_blank'>privacy policy</a></p>
                         <div className={styles.submitBtnCntr}>
                             <input type='submit' className={styles.submitBtn} value={loading ? 'submitting' : 'submit'} />
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="19" viewBox="0 0 25 19" fill="none">
