317f6201844b7c94fd67891f307d2e9ec52e56b5
LMLHOMESLLP <157579608+LMLHOMESLLP@users.noreply.github.com>
2024-10-14 17:28:03 +0530

    OtpFormNew.js

--- components/OtpFormNew.js
+++ components/OtpFormNew.js
@@ -110,7 +110,7 @@ export default function OtpFormNew({ closeForm, onFormSubmit }) {
                                 <p className="verifyTxt">Verify Your Phone Number</p>
 
                                 <PhoneInput country={"in"} value={phoneNumber} onChange={setPhoneNumber} />
-<p style={{fontSize: '11px'}}><img src='https://www.lmlhomes.in/assets/images/tick-mark.jpg' height={12} /> I authorise LML Homes LLP & its representatives to contact me with updates and notifications via Email / SMS /WhatsApp / Call. This will override DND/NDNC. I accept the <a href='https://www.lmlhomes.in/privacy-policy' target='_blank'>privacy policy</a></p>
+<p style={{fontSize: '11px', color: '#fff', width: '300px', marginTop: '10px'}}><img src='https://www.lmlhomes.in/assets/images/tick-mark.jpg' height={12} /> I authorise LML Homes LLP & its representatives to contact me with updates and notifications via Email / SMS /WhatsApp / Call. This will override DND/NDNC. I accept the <a href='https://www.lmlhomes.in/privacy-policy' target='_blank'>privacy policy</a></p>
                                 <button
                                     onClick={handleSendCode}
                                     id="send-code-button"
