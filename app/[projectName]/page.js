6ce1f9977f17dee3b3d8f87872ddcb11323b0542
LMLHOMESLLP <157579608+LMLHOMESLLP@users.noreply.github.com>
2024-10-22 12:51:09 +0530

    page.js

--- app/[projectName]/page.js
+++ app/[projectName]/page.js
@@ -6,6 +6,12 @@ import HomeSections from "./HomeSections";
 import LandingPageForm from "@/components/LandingPageForm";
 import { usePathname } from "next/navigation";
 import { overAllData } from "@/utils/constants";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
 export default function Projects() {
   const pathname = usePathname();
 
@@ -20,7 +26,7 @@ export default function Projects() {
 
     if (home1Elem) {
       if (isMobile) {
-         if (pathname == "/league-one") {
+        if (pathname == "/league-one") {
           home1Elem.style.background =
             "linear-gradient(180deg, rgba(0,0,0,0.9024203431372549) 0%, rgba(0,0,0,0.08729429271708689) 0%, rgba(0,0,0,0.4038209033613446) 72%, rgba(0,0,0,1) 100%),url(/assets/images/banners/leagueone-mbl.png)";
         } else if (pathname == "/prakriti") {
@@ -68,9 +74,9 @@ export default function Projects() {
     <section className={styles.projSection}>
       {Object.keys(projectDetailPages).length > 0 ? (
         <>
-          <div className={styles.home1} id="bg">
+          <div className={styles.home1} id={`${pathname === "/league-one" ? null : "bg"}`}>
             {projectDetailPages?.bannerHighlights?.length > 0 && (
-              <div className={styles.homeHighlight}>
+              <div className={styles.homeHighlight} style={{ zIndex: "10" }}>
                 {projectDetailPages?.bannerHighlights?.map((highlights, index, arr) => (
                   <>
                     <div className={styles.highlightbox} key={index}>
@@ -84,8 +90,74 @@ export default function Projects() {
                 ))}
               </div>
             )}
+            {pathname === "/league-one" && (
+              <div className="position-absolute top-0 w-100 h-100 bg-white">
+                <div>
+                  <Swiper
+                    modules={[Navigation, Autoplay]}
+                    navigation={true}
+                    slidesPerView={1}
+                    autoplay={{
+                      delay: 10000,
+                    }}
+                    breakpoints={{
+                      0: {
+                        slidesPerView: 1,
+                      },
+                      720: {
+                        slidesPerView: 1,
+                      },
+                      1024: {
+                        slidesPerView: 1,
+                      },
+                    }}
+                  >
+                    <SwiperSlide>
+                      <div className="position-relative">
+                        <img
+                          src="/assets/images/league-one-promo-banner.jpg"
+                          alt="s"
+                          className="league-one-desk-banr"
+                          style={{ width: "100%", height: "100vh", objectFit: "cover" }}
+                        />
+                        <img
+                          src="/assets/images/league-one-promo-mob-banner.jpg"
+                          alt="s"
+                          className="league-one-mob-banr"
+                          style={{ width: "100%", height: "80vh", objectFit: "cover" }}
+                        />
+                        <div
+                          style={{ backgroundColor: "#00000038" }}
+                          className="position-absolute top-0 w-100 h-100"
+                        ></div>
+                      </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="position-relative">
+                        <img
+                          src="/assets/images/league-one-banner.jpg"
+                          alt="s"
+                          className="league-one-desk-banr"
+                          style={{ width: "100%", height: "100vh", objectFit: "cover" }}
+                        />
+                        <img
+                          src="/assets/images/League-One-Mobile Banner .png"
+                          alt="s"
+                          className="league-one-mob-banr"
+                          style={{ width: "100%", height: "80vh", objectFit: "cover" }}
+                        />
+                        <div
+                          style={{ backgroundColor: "#00000038" }}
+                          className="position-absolute top-0 w-100 h-100"
+                        ></div>
+                      </div>
+                    </SwiperSlide>
+                  </Swiper>
+                </div>
+              </div>
+            )}
           </div>
-
+          {/* hello */}
           <HomeSections projectDetailPages={projectDetailPages} />
           <div className={styles.homeEnquire}>
             {/* {projectDetailPages?.enquire?.phone && (
