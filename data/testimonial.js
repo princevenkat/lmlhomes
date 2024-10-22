b6ab9d17085ef53379f6878fc81f0ec8011e10a4
LMLHOMESLLP <157579608+LMLHOMESLLP@users.noreply.github.com>
2024-10-22 16:08:40 +0530

    testimonial.js

--- data/testimonial.js
+++ data/testimonial.js
@@ -6,13 +6,16 @@ export const testimonial = [
         description: 'We own a 3 BHK lifestyle apartment at Atita, a luxury project by LML Homes at Perungudi. The Apartment is worth every penny and the construction quality is good. The ethical work culture of LML Homes and the timely delivery by the LML Team headed by Mr, Mahesh Kothari has impressed us immensely. I will surely recommend prospective home buyers to buy homes from LML Homes',
         image: '/assets/images/testimonial-4.png',
         alt: 'Mr. Mohandass testimony',
+        video: ''
     },
     {
         name: 'Mehar Nisha',
         location: 'Tambaram',
         property: 'LML Prakriti',
         description: 'We would like to thank and appreciate the entire LML Homes team for the wonderful work and architecture of the LML Prakriti premises. We would like to thank Ilairaja for all the corporation and for working on our feedback effectively. We would also like to thank Rajesh and Dimple for their documentation support. Additionally, we would like to thank Vignesh and MD Mr. Mahesh Kothari for the explanation of the project very clear. Thanks to the entire LML Team. You guys are the best',
-        image: '/assets/images/testimonial-2.png'
+        image: '/assets/images/testimonial-2.png',
+        alt: 'Mehar Nisha testimony',
+        video: ''
     },
     {
         name: 'Mrs. Vanitha',
@@ -21,6 +24,7 @@ export const testimonial = [
         description: 'I am so glad to buy a house with LML Raga. I am an Architect who takes a lot of time in minute details before finalizing a house. But, the decision to buy this house with LML Raga was finalized just 2 days after seeing the property. I must appreciate the efficiency of your staff too. Thank you for making this transition seamless. I highly recommend LML Raga.',
         image: '/assets/images/testimonial-1.png',
         alt: 'Mrs. Vanitha testimony',
+        video: ''
     },
     {
         name: 'Mr. Suman,',
@@ -29,6 +33,7 @@ export const testimonial = [
         description: 'Thanks much to LML Homes for helping me out to get a nice flat with expected Vaastu. I got this information from 99acres and the sales team shared the details and clarified my doubts very patiently. Thanks again to LML Homes for getting me nice flat near to my office MEPZ.',
         image: '/assets/images/testimonial-3.png',
         alt: 'Mr. Suman testimony',
+        video: ''
     },
     {
         name: 'Ms. K. Preethi',
@@ -37,6 +42,34 @@ export const testimonial = [
         description: 'I would like to express my thanks for good communication and coordination for the registration. The site is pretty awesome and good and I am happy to have a sweet home from "LML Homes". The location of property is the very plus point, have ease of access to all the routes in Chennai. I recommend "LML Homes" based on kind attitude, communication and coordination from LML Team.',
         image: '/assets/images/testimonial-5.png',
         alt: 'Ms. K. Preethi testimony',
+        video: ''
+    },
+    {
+        name: 'Mr. Srinivasan',
+        location: '',
+        property: 'LML League One',
+        description: '',
+        image: '',
+        alt: 'Mr. Srinivasan testimony',
+        video: 'https://www.youtube.com/embed/v5jSn23WuUk?si=4Fcv4lklhuDW97ZB'
+    },
+    {
+        name: 'Mr. Jagadeesh',
+        location: '',
+        property: 'LML League One',
+        description: '',
+        image: '',
+        alt: 'Mr. Jagadeesh testimony',
+        video: 'https://www.youtube.com/embed/us2IfHMZxNE?si=W7sFnD6GWXdhJIF9'
+    },
+    {
+        name: 'Mr. Arun',
+        location: '',
+        property: 'LML League One',
+        description: '',
+        image: '',
+        alt: 'Mr. Arun testimony',
+        video: 'https://www.youtube.com/embed/SeAS5bI1vII?si=xE4HH2fHDsbJkPT7'
     },
 
 ]
