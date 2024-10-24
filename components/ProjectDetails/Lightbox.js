5026b69c938e566de0b7fc50697331beb1743923
LMLHOMESLLP <157579608+LMLHOMESLLP@users.noreply.github.com>
2024-10-24 13:02:12 +0530

    Lightbox.js

---
+++ components/ProjectDetails/Lightbox.js
@@ -0,0 +1,6 @@
+import LightboxComponent from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+export default function Lightbox(props) {
+  return <LightboxComponent {...props} />;
+}
