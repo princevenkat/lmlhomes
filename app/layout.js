cb0360455834e636dcd124f2ee54907ec6bea798
Sai Krishna <pk@lmlhomes.in>
2024-10-25 16:23:09 +0530

    Updated RootLayout with Google Tag Manager integration

--- app/layout.js
+++ app/layout.js
@@ -1,54 +1,87 @@
-'use client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './globals.css'
-import './responsive.css'
-import '../styles/slick.css'
-import '../styles/slick-theme.css'
-import 'swiper/css'
-import 'swiper/css/bundle'
-import 'swiper/css/pagination'
-import 'swiper/css/navigation'
-import 'react-18-image-lightbox/style.css'
-import Header from '@/components/Header'
-import MobHeader from '@/components/MobHeader'
-import Footer from '@/components/Footer'
-import Script from 'next/script'
-import Head from 'next/head'
-import { motion } from 'framer-motion'
-
-
+"use client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./globals.css";
+import "./responsive.css";
+import "../styles/slick.css";
+import "../styles/slick-theme.css";
+import "swiper/css";
+import "swiper/css/bundle";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "react-18-image-lightbox/style.css";
+import Header from "@/components/Header";
+import MobHeader from "@/components/MobHeader";
+import Footer from "@/components/Footer";
+import Script from "next/script";
+import Head from "next/head";
+import { motion } from "framer-motion";
 
 export default function RootLayout({ children }) {
-
-
   return (
-    <html lang='en'>
+    <html lang="en">
+      <Head>
+        {/* Google Tag Manager - Head */}
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T5S96K9K');`,
+          }}
+        />
+
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css"
+          integrity="sha512-wR4oNhLBHf7smjy0K4oqzdWumd+r5/+6QO/vDda76MW5iug4PT7v86FoEkySIJft3XA0Ae6axhIvHrqwm793Nw=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css"
+          integrity="sha512-6lLUdeQ5uheMFbWm3CP271l14RsX1xtx+J5x2yeIDkkiBpeVTNhTqijME7GgRKKi6hCqovwCoBTlRBEC20M8Mg=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+
+        <link rel="icon" href="favicon.ico" type="image/x-icon" sizes="any" />
+      </Head>
 
       <body suppressHydrationWarning={true}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5S96K9K"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
 
-        <Head>
-          <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css'
-            integrity='sha512-wR4oNhLBHf7smjy0K4oqzdWumd+r5/+6QO/vDda76MW5iug4PT7v86FoEkySIJft3XA0Ae6axhIvHrqwm793Nw=='
-            crossorigin='anonymous' referrerpolicy='no-referrer' />
-          <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css'
-            integrity='sha512-6lLUdeQ5uheMFbWm3CP271l14RsX1xtx+J5x2yeIDkkiBpeVTNhTqijME7GgRKKi6hCqovwCoBTlRBEC20M8Mg=='
-            crossorigin='anonymous' referrerpolicy='no-referrer' />
+        <Script
+          src="https://code.jquery.com/jquery-3.7.0.js"
+          integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM="
+          crossOrigin="anonymous"
+          strategy="beforeInteractive"
+        />
 
-          <Script src='https://code.jquery.com/jquery-3.7.0.js'
-            integrity='sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=' crossorigin='anonymous'
-            strategy='beforeInteractive' />
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"
+          integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+          strategy="beforeInteractive"
+        />
 
-          <Script src='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js'
-            integrity='sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg=='
-            crossorigin='anonymous' referrerpolicy='no-referrer' strategy='beforeInteractive' />
+        <Script src="https://rawgit.com/miso25/mapoid/master/mapoid.min.js?1" />
 
-          <Script src="https://rawgit.com/miso25/mapoid/master/mapoid.min.js?1" />
-
-          <Script
-            id="fb-pixel"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `!function(f,b,e,v,n,t,s)
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -58,30 +91,19 @@ export default function RootLayout({ children }) {
               'https://connect.facebook.net/en_US/fbevents.js');
               fbq('init', '1303789746642476');
               fbq('track', 'PageView');`,
-            }} />
-
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1303789746642476&ev=PageView&noscript=1" />`,
-            }}
-          />
-
-          <noscript><img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=1303789746642476&ev=PageView&noscript=1"
-          /></noscript>
-
-
-          <link rel="icon" href="favicon.ico" type="image/x-icon" sizes="any" />
-
-        </Head>
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1303789746642476&ev=PageView&noscript=1" />`,
+          }}
+        />
 
         <MobHeader />
         <Header />
         {children}
         <Footer />
-
       </body>
-
     </html>
-  )
+  );
 }
