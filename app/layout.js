6a013bfe3058f15d634e6eb3775ab3eff2d033e1
Sai Krishna <pk@lmlhomes.in>
2024-10-30 12:10:25 +0530

    Google analytics tag

--- app/layout.js
+++ app/layout.js
@@ -32,6 +32,32 @@ export default function RootLayout({ children }) {
             })(window,document,'script','dataLayer','GTM-T5S96K9K');`,
           }}
         />
+<Script/>
+<Script
+  id="google-tag-manager"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','G-DYY096BPP5');
+    `,
+  }}
+/>
+<Script
+  id="google-tag-manager-config"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-DYY096BPP5');
+    `,
+  }}
+/>
 
         <link
           rel="stylesheet"
