90bcaa72e31be49ef32caddb4c17b5d2e3d2f483
Sai Krishna <pk@lmlhomes.in>
2024-11-08 14:00:50 +0530

    Gtag code Updated

--- app/layout.js
+++ app/layout.js
@@ -20,13 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.GTAG_ID}');`,
-          }}
-        />
+        
 
         <link
           rel="stylesheet"
@@ -55,6 +49,13 @@ export default function RootLayout({ children }) {
             style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.GTAG_ID}');`,
+          }}
+        />
 
         <Script
           src="https://code.jquery.com/jquery-3.7.0.js"
