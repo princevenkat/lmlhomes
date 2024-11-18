c4c481c724080efefdaea805be0831553fe1c17d
Sai Krishna <pk@lmlhomes.in>
2024-11-18 16:14:39 +0530

    Alt Atributes

--- utils/constants.js
+++ utils/constants.js
@@ -736,7 +736,7 @@ const overAllData = {
             description:
               "We own a 3 BHK lifestyle apartment at Atita, a luxury project by LML Homes at Perungudi. The Apartment is worth every penny and the construction quality is good. The ethical work culture of LML Homes and the timely delivery by the LML Team headed by Mr, Mahesh Kothari has impressed us immensely. I will surely recommend prospective home buyers to buy homes from LML Homes",
             image: "/assets/images/testimonial-4.png",
-            alt: "Mr. Mohandass testimony",
+            alt: "Testimonial from Mohandass about LML Homes' excellent service",
           },
           {
             name: "Mehar Nisha",
@@ -745,6 +745,7 @@ const overAllData = {
             description:
               "We would like to thank and appreciate the entire LML Homes team for the wonderful work and architecture of the LML Prakriti premises. We would like to thank Ilairaja for all the corporation and for working on our feedback effectively. We would also like to thank Rajesh and Dimple for their documentation support. Additionally, we would like to thank Vignesh and MD Mr. Mahesh Kothari for the explanation of the project very clear. Thanks to the entire LML Team. You guys are the best",
             image: "/assets/images/testimonial-2.png",
+        alt: "Mehar Nisha's review highlighting LML Homes' top-notch quality",
           },
           {
             name: "Mrs. Vanitha",
@@ -753,7 +754,8 @@ const overAllData = {
             description:
               "I am so glad to buy a house with LML Raga. I am an Architect who takes a lot of time in minute details before finalizing a house. But, the decision to buy this house with LML Raga was finalized just 2 days after seeing the property. I must appreciate the efficiency of your staff too. Thank you for making this transition seamless. I highly recommend LML Raga.",
             image: "/assets/images/testimonial-1.png",
-            alt: "Mrs. Vanitha testimony",
+        alt: "Feedback from Vanitha on her new home at LML Homes",
+            
           },
           {
             name: "Mr. Suman,",
@@ -762,7 +764,7 @@ const overAllData = {
             description:
               "Thanks much to LML Homes for helping me out to get a nice flat with expected Vaastu. I got this information from 99acres and the sales team shared the details and clarified my doubts very patiently. Thanks again to LML Homes for getting me nice flat near to my office MEPZ.",
             image: "/assets/images/testimonial-3.png",
-            alt: "Mr. Suman testimony",
+        alt: "Review by Suman praising LML Homes' exceptional customer support",            
           },
           {
             name: "Ms. K. Preethi",
@@ -771,7 +773,7 @@ const overAllData = {
             description:
               'I would like to express my thanks for good communication and coordination for the registration. The site is pretty awesome and good and I am happy to have a sweet home from "LML Homes". The location of property is the very plus point, have ease of access to all the routes in Chennai. I recommend "LML Homes" based on kind attitude, communication and coordination from LML Team.',
             image: "/assets/images/testimonial-5.png",
-            alt: "Ms. K. Preethi testimony",
+        alt: "Feedback from K. Preethi about the seamless home-buying experience with LML Homes",
           },
         ],
         specifications: [
