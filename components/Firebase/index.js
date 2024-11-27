32455b84f6f275d66505407a47a60ff63b93a56a
Sai Krishna <pk@lmlhomes.in>
2024-11-27 16:16:37 +0530

    OTP-Firebase

--- components/Firebase/index.js
+++ components/Firebase/index.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyD2HrSmh_ySCw3ld4Ol5Sjhwb7ueL9IU8o",
-    authDomain: "lmlhomesotpintegration.firebaseapp.com",
-    projectId: "lmlhomesotpintegration",
-    storageBucket: "lmlhomesotpintegration.appspot.com",
-    messagingSenderId: "324690088277",
-    appId: "1:324690088277:web:52debe7ffc75f57f001fdc",
-    measurementId: "G-LBT5HBN06G"
+  authDomain: "lmlhomesotpintegration.firebaseapp.com",
+  projectId: "lmlhomesotpintegration",
+  storageBucket: "lmlhomesotpintegration.firebasestorage.app",
+  messagingSenderId: "324690088277",
+  appId: "1:324690088277:web:52debe7ffc75f57f001fdc",
+  measurementId: "G-LBT5HBN06G"
 };
 
 if (!firebase.apps.length) {
